4b5be6cfb7bebe31ab90bd5fa8bb7491fa56195e
abdullahb53 <abdullahb5355@gmail.com>
2022-04-12 12:06:20 +0300

    voidtime-duzeldi

--- main.cpp
+++ main.cpp
@@ -18,53 +18,60 @@
 
 #include <stdio.h>
 #include <omp.h>
+#include <sys/time.h>
 
 using namespace std;
-
-
-
-unsigned int N = 100000;
-unsigned int count=0;
-
-
-bool isPalindrome(unsigned n)
+int N = 100000;
+int count_e = 0;
+//binary tersi bulup dogrudan karsilastirip degeri bulabiliyor.
+bool plandormmu(unsigned n)
 {
-    unsigned rev = 0;
-    unsigned k = n;
-    while (k)
+    unsigned ters = 0;
+    unsigned k=n;
+    while(k)
     {
-        rev = (rev << 1) | (k & 1);
-        k = k >> 1;     // drop last bit
+        //ters binary format olusturuldu
+        ters = (ters << 1)| (k&1);
+        //k saga kayarak tersi bir sonraki dongude oldurmaya vesile oluyor
+        k = k >> 1;
     }
-    return n == rev;
+
+    return ters == n;
 }
 
 
 void decToBinary(int n)
 {
     omp_set_num_threads(8);
-    #pragma omp parallel for schedule(static)
-    for(int k = 0; k<n; k++)
+
+    #pragma omp parallel for schedule(dynamic)
+    for(unsigned int k = 0; k<n; k++)
     {
-
-        if(isPalindrome(k) == false ){
-
-        }
-        else{
+        if(plandormmu(k) == true ){
             #pragma omp critical
             {
-                count++;
+                count_e++;
             }
         }
-
     }
 
 }
 
 int main()
 {
-        decToBinary(N-1);
-     printf("\n countReal= %d",count);
+
+    timeval tim;
+    gettimeofday(&tim,NULL);
+    double t1 = 1.0e6 * tim.tv_sec + tim.tv_usec;
+    //FUNC
+    decToBinary(N);
+    //FUNC
+    gettimeofday(&tim,NULL);
+    double t2 = 1.0e6 * tim.tv_sec + tim.tv_usec;
+
+    double facelessVoid = (t2-t1);
+
+    printf("\n countReal= %d ______ VoidTime = %.0f",count_e,facelessVoid);
     getchar();
     return 0;
 }
